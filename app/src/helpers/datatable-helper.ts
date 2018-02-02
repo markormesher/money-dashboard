1c4b6ac1095c458a41d95063963b6f1e3093af4f
Mark Ormesher <me@markormesher.co.uk>
2018-02-02 14:07:12 +0000

    Clean up code in datatable helper

--- app/src/helpers/datatable-helper.ts
+++ app/src/helpers/datatable-helper.ts
@@ -11,25 +11,19 @@ interface DatatableResponse<T> {
 // TODO: figure out what type "model" is meant to be
 
 function getData<T>(model: any, req: Request, countFilter: IFindOptions<T>, dataFilter: IFindOptions<T>): Bluebird<DatatableResponse<T>> {
-
-	const offset = parseInt(req.query['start']);
-	const limit = parseInt(req.query['length']);
-
 	const columns: { data: string }[] = req.query['columns'];
 	const rawOrder: { column: number, dir: string }[] = req.query['order'];
-	const order: string[][] = rawOrder.map((rawOrderItem) => {
+
+	dataFilter.offset = parseInt(req.query['start']);
+	dataFilter.limit = parseInt(req.query['length']);
+	dataFilter.order = rawOrder.map((rawOrderItem) => {
 		return [columns[rawOrderItem.column].data, rawOrderItem.dir];
 	});
-
-	dataFilter.order = order;
-	dataFilter.offset = offset;
-	dataFilter.limit = limit;
 
 	return Bluebird.all([
 		model.count(countFilter),
 		model.findAll(dataFilter),
-	]).then(results => {
-		const [totalCount, data] = results;
+	]).spread((totalCount: number, data: T[]) => {
 		return {
 			recordsTotal: totalCount,
 			recordsFiltered: data.length,
