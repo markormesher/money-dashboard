435d6307be67772c319656c3c9f0b61d4d1bbf16
Mark Ormesher <me@markormesher.co.uk>
2018-08-04 09:18:01 +0100

    WIP

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import BrowserRouter from "react-router-dom/es/BrowserRouter";
-import './index.css';
-import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+
+import App from "./components/App/App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import "./index.css";
 
 ReactDOM.render((
 		<BrowserRouter>
 			<App/>
 		</BrowserRouter>
-), document.getElementById('root'));
+), document.getElementById("root"));
 
-if (process.env.ENV === "dev") {
-	registerServiceWorker();
-}
+registerServiceWorker();
