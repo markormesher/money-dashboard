c5bcfb87fd6f2b0422345a5897668fa780ee59a1
Mark Ormesher <me@markormesher.co.uk>
2018-09-17 18:01:54 +0100

    Less awful looking login page

--- src/client/components/Login/Login.tsx
+++ src/client/components/Login/Login.tsx
@@ -1,12 +1,25 @@
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import cn = require("classnames");
 import * as React from "react";
 import { Component } from "react";
+
+import * as bs from "bootstrap/dist/css/bootstrap.css";
+import * as style from "./Login.scss";
 
 class Login extends Component {
 	public render() {
 		return (
-				<div>
-					<div>Login</div>
-					<a href="/auth/google/login">Login with Google</a>
+				<div className={style.loginWrapper}>
+					<div className={style.titleWrapper}>
+						<h1>Login</h1>
+					</div>
+					<form action="/auth/google/login" method="get">
+						<button role="submit" className={cn(bs.btn, bs.btnOutlinePrimary, bs.mt4)}>
+							<FontAwesomeIcon icon={faGoogle} className={bs.mr2}/>
+							Login with Google
+						</button>
+					</form>
 				</div>
 		);
 	}
