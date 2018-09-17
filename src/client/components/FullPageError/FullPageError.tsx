b3d93435137faf2300cf5ad2cb3720d08a20e280
Mark Ormesher <me@markormesher.co.uk>
2018-09-17 18:56:29 +0100

    Make the error page look less-awful

--- src/client/components/FullPageError/FullPageError.tsx
+++ src/client/components/FullPageError/FullPageError.tsx
@@ -1,8 +1,11 @@
 import { faExclamationTriangle } from "@fortawesome/pro-light-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import cn = require("classnames");
 import * as React from "react";
 import { Component } from "react";
 import { DetailedError } from "../../helpers/errors/DetailedError";
+
+import * as bs from "bootstrap/dist/css/bootstrap.css";
 import * as styles from "./FullPageError.scss";
 
 interface IFullPageErrorProps {
@@ -13,21 +16,18 @@ class FullPageError extends Component<IFullPageErrorProps> {
 
 	public render() {
 		const errorMessage = this.props.error.message || "Something went wrong";
-		const errorDescription = this.props.error instanceof DetailedError ? this.props.error.description : undefined;
+		const errorDescription = this.props.error instanceof DetailedError && this.props.error.description;
 
 		return (
 				<div className={styles.errorWrapper}>
-					<div>
-						<h2>
-							<FontAwesomeIcon icon={faExclamationTriangle}/>
-							{errorMessage}
-						</h2>
-						{errorDescription ? <p>{errorDescription}</p> : undefined}
-						<p>
-							You might want to try reloading the page,
-							or <a href="/">going back to your dashboard</a>.
-						</p>
-					</div>
+					<h2>
+						<FontAwesomeIcon icon={faExclamationTriangle} className={cn(bs.mr2, bs.textMuted)}/>
+						{errorMessage}
+					</h2>
+
+					{errorDescription && <p>{errorDescription}</p>}
+
+					<p>You might want to try reloading the page, or <a href="/">going back to your dashboard</a>.</p>
 				</div>
 		);
 
