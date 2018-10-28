1b88ac4c20bd0d07d85f32bc799cb010c7639b5f
Mark Ormesher <me@markormesher.co.uk>
2018-10-28 10:14:47 +0000

    Shave off a few bytes

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ const {resolve, join} = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserWebpackPlugin = require("terser-webpack-plugin");
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const md5 = require("md5");
 
+const notFalse = (val) => val !== false;
 const IS_PROD = process.env.NODE_ENV === "production";
 const IS_DEV = !IS_PROD; // for better readability below
+
+const outputDir = resolve(__dirname, "build", "client");
 
 const babelLoader = {
 	loader: 'babel-loader',
@@ -54,7 +59,7 @@ const terserMinimiser = new TerserWebpackPlugin({
 		},
 		mangle: {
 			properties: {
-				regex: /^[A-Z_]+$/,
+				regex: /^[A-Z]+_[A-Z_]+$/, // should only match redux actions
 			},
 		},
 	},
@@ -67,10 +72,10 @@ module.exports = {
 	entry: resolve(__dirname, "src", "client", "index.tsx"),
 	output: {
 		publicPath: "/",
-		path: resolve(__dirname, "build", "client"),
+		path: outputDir,
 		filename: "[name]~bundle.js",
 
-		// used for in development mode only
+		// used in development mode only
 		hotUpdateMainFilename: "hot-update.[hash:6].json",
 		hotUpdateChunkFilename: "hot-update.[hash:6].js",
 	},
@@ -136,12 +141,27 @@ module.exports = {
 			minify: IS_PROD,
 			alwaysWriteToDisk: IS_DEV,
 		}),
+		new ReplaceInFileWebpackPlugin([{
+			dir: outputDir,
+			rules: [
+				{
+					// remove line breaks and spaces in Font Awesome pre-minified output
+					search: /(\\n+)( *)/g,
+					replace: "",
+				},
+				IS_PROD && {
+					// replace redux action strings with hashes
+					search: /"([A-Za-z]+)Actions\.([_A-Z]+)"/g,
+					replace: (str) => "\"" + md5(str).substring(0, 6) + "\"",
+				},
+			].filter(notFalse),
+		}]),
 		IS_PROD && new BundleAnalyzerPlugin({
 			analyzerMode: "static",
 			openAnalyzer: false,
 		}),
 		IS_DEV && new webpack.HotModuleReplacementPlugin(),
-	].filter((p) => p !== false),
+	].filter(notFalse),
 	resolve: {
 		extensions: [".js", ".jsx", ".ts", ".tsx"],
 		modules: ["node_modules", join("src", "client")],
