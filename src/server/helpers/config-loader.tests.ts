86017281323ce16f86769552445aab00180a4693
Mark Ormesher <me@markormesher.co.uk>
2018-12-24 07:05:05 +0000

    Fix tests

--- src/server/helpers/config-loader.tests.ts
+++ src/server/helpers/config-loader.tests.ts
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import { after, afterEach, before, describe, it } from "mocha";
+import { after, afterEach, beforeEach, describe, it } from "mocha";
 import * as sinon from "sinon";
 import {
 	clearConstantsCache,
@@ -105,54 +105,40 @@ describe(__filename, () => {
 
 	describe("getSecret() and clearSecretsCache()", () => {
 
-		let didAddRunningInEnvVar = false;
+		// TODO: remove this stubbing when we start running tests in docker
 
-		before(() => {
-			if (!process.env.RUNNING_IN) {
-				didAddRunningInEnvVar = true;
-				process.env.RUNNING_IN = "";
-			}
-		});
+		let fsStub: sinon.SinonStub;
 
-		after(() => {
-			if (didAddRunningInEnvVar) {
-				delete process.env.RUNNING_IN;
-			}
-		});
-
-		afterEach(() => {
-			clearSecretsCache();
-		});
-
-		it("should read a secret file without error when not running in docker", () => {
-			sandbox.replace(process.env, "RUNNING_IN", "not-docker");
-			getSecret("test.secret").should.equal("test-secret");
-		});
-
-		it("should read a secret file from the correct path when running in docker", () => {
-			sandbox.replace(process.env, "RUNNING_IN", "docker");
-			sandbox.stub(fs, "readFileSync").callsFake((...args) => {
+		beforeEach(() => {
+			fsStub = sandbox.stub(fs, "readFileSync").callsFake((...args) => {
 				args[0].should.equal("/run/secrets/test.secret");
 				return "test-secret";
 			});
-			getSecret("test.secret").should.equal("test-secret");
+		});
+
+		afterEach(() => {
+			fsStub.reset();
+			clearSecretsCache();
+		});
+
+		it("should call the file system to read a secret file", () => {
+			getSecret("test.secret");
+			fsStub.callCount.should.equal(1);
 		});
 
 		it("should use cached secrets when running more than once", () => {
-			const stub = sandbox.stub(fs, "readFileSync").callThrough();
 			getSecret("test.secret");
-			stub.callCount.should.equal(1);
+			fsStub.callCount.should.equal(1);
 			getSecret("test.secret");
-			stub.callCount.should.equal(1);
+			fsStub.callCount.should.equal(1);
 		});
 
 		it("should re-read the file when the secrets cache is cleared", () => {
-			const stub = sandbox.stub(fs, "readFileSync").callThrough();
 			getSecret("test.secret");
-			stub.callCount.should.equal(1);
+			fsStub.callCount.should.equal(1);
 			clearSecretsCache();
 			getSecret("test.secret");
-			stub.callCount.should.equal(2);
+			fsStub.callCount.should.equal(2);
 		});
 	});
 
