343b721518410b6218e11bc6a7347387bc9efabc
Mark Ormesher <me@markormesher.co.uk>
2018-12-28 10:43:45 +0000

    Move wait-for-it to npm

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./docker/app/wait-for-it.sh postgres_master:5432 --timeout=30 --strict &
-./docker/app/wait-for-it.sh postgres_slave_1:5432 --timeout=30 --strict &
-./docker/app/wait-for-it.sh postgres_slave_2:5432 --timeout=30 --strict &
-./docker/app/wait-for-it.sh redis:6379 --timeout=30 --strict &
+./node_modules/.bin/wait-for-it postgres_master:5432 --timeout=30 --strict &
+./node_modules/.bin/wait-for-it postgres_slave_1:5432 --timeout=30 --strict &
+./node_modules/.bin/wait-for-it postgres_slave_2:5432 --timeout=30 --strict &
+./node_modules/.bin/wait-for-it redis:6379 --timeout=30 --strict &
 
 wait
 
