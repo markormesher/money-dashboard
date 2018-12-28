12d2e979725a3cc6928e52d8eb3de993e67a58b4
Mark Ormesher <me@markormesher.co.uk>
2018-12-28 10:36:53 +0000

    Move postgres slave entry files; remove git clone for wait-for-it dependency

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./wait-for-it/wait-for-it.sh postgres_master:5432 --timeout=30 --strict &
-./wait-for-it/wait-for-it.sh postgres_slave_1:5432 --timeout=30 --strict &
-./wait-for-it/wait-for-it.sh postgres_slave_2:5432 --timeout=30 --strict &
-./wait-for-it/wait-for-it.sh redis:6379 --timeout=30 --strict &
+./docker/app/wait-for-it.sh postgres_master:5432 --timeout=30 --strict &
+./docker/app/wait-for-it.sh postgres_slave_1:5432 --timeout=30 --strict &
+./docker/app/wait-for-it.sh postgres_slave_2:5432 --timeout=30 --strict &
+./docker/app/wait-for-it.sh redis:6379 --timeout=30 --strict &
 
 wait
 
