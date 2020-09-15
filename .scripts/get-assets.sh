0968e5aa304a40b5a7e0e76963ca629eb485be2c
Mark Ormesher <me@markormesher.co.uk>
2020-09-15 09:56:42 +0100

    move IPs to settings file

--- .scripts/get-assets.sh
+++ .scripts/get-assets.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "${script_dir}/remote-settings.sh"
 
 assets=(
   fortawesome-pro-light-svg-icons-5.14.0.tgz
@@ -9,5 +10,5 @@ assets=(
 
 for asset in ${assets[@]}; do
     echo "Getting ${asset}"
-  rsync -avz "deploy@142.93.47.93:/var/web/assets/${asset}" "${script_dir}/../assets/."
+  rsync -avz "deploy@142.93.47.93:${REMOTE_ASSET_DIR}/${asset}" "${script_dir}/../assets/."
 done
