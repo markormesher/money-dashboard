5aa0aef48ba9c680781d90938a3025ff4e00d5d5
Mark Ormesher <me@markormesher.co.uk>
2020-09-15 09:37:35 +0100

    add asset script

---
+++ .scripts/get-assets.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+assets=(
+  fortawesome-pro-light-svg-icons-5.14.0.tgz
+)
+
+for asset in ${assets[@]}; do
+    echo "Getting ${asset}"
+  scp "deploy@142.93.47.93:/var/web/assets/${asset}" "${script_dir}/../assets/."
+done
