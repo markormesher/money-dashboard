cf7682fabd9c18a3426332df78da0e83391a258e
Mark Ormesher <me@markormesher.co.uk>
2020-09-16 13:42:12 +0100

    cron auth

--- src/api/controllers/exchange-rates.ts
+++ src/api/controllers/exchange-rates.ts
@@ -1,6 +1,6 @@
 import * as Express from "express";
 import { NextFunction, Request, Response } from "express";
-import { requireUser } from "../middleware/auth-middleware";
+import { requireUser, requireCronAuth } from "../middleware/auth-middleware";
 import {
   getLatestExchangeRates,
   updateLatestExchangeRates,
@@ -15,16 +15,14 @@ router.get("/latest", requireUser, (req: Request, res: Response, next: NextFunct
     .catch(next);
 });
 
-// TODO: cron auth
-router.post("/update-historical", (req: Request, res: Response, next: NextFunction) => {
+router.post("/update-historical", requireCronAuth, (req: Request, res: Response, next: NextFunction) => {
   const days = parseInt(req.query["days"] as string) || 2;
   updateHistoricalExchangeRages(days)
     .then(() => res.status(200).send())
     .catch(next);
 });
 
-// TODO: cron auth
-router.post("/update-latest", (req: Request, res: Response, next: NextFunction) => {
+router.post("/update-latest", requireCronAuth, (req: Request, res: Response, next: NextFunction) => {
   updateLatestExchangeRates()
     .then(() => res.status(200).send())
     .catch(next);
