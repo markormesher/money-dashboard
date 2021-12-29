58c8093bc5609bd370cea8d10687f34716d769ef
Mark Ormesher <me@markormesher.co.uk>
2021-12-29 17:36:00 +0000

    remove postgres replication

--- src/api/db/db-config.ts
+++ src/api/db/db-config.ts
@@ -8,28 +8,10 @@ const typeormConf: ConnectionOptions = {
   type: "postgres",
   logging: isDev() ? "all" : false,
   namingStrategy: new PostgresNamingStrategy(),
-  replication: {
-    master: {
-      host: "postgres_primary",
-      username: "money_dashboard",
-      password: getSecret("postgres.password"),
-      database: isTest() ? "money_dashboard_test" : "money_dashboard",
-    },
-    slaves: [
-      {
-        host: "postgres_replica_1",
-        username: "money_dashboard",
-        password: getSecret("postgres.password"),
-        database: isTest() ? "money_dashboard_test" : "money_dashboard",
-      },
-      {
-        host: "postgres_replica_2",
-        username: "money_dashboard",
-        password: getSecret("postgres.password"),
-        database: isTest() ? "money_dashboard_test" : "money_dashboard",
-      },
-    ],
-  },
+  host: "postgres_primary",
+  username: "money_dashboard",
+  password: getSecret("postgres.password"),
+  database: isTest() ? "money_dashboard_test" : "money_dashboard",
   entities: [join(__dirname, "models", "*.js")],
 };
 
