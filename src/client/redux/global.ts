83f94a21c2dde4a42546cd53a9eebedd693a5ac8
Mark Ormesher <me@markormesher.co.uk>
2024-02-18 18:12:41 +0000

    un-redux key shortcut modal

--- src/client/redux/global.ts
+++ src/client/redux/global.ts
@@ -3,30 +3,20 @@ import { PayloadAction } from "./helpers/PayloadAction";
 
 interface IGlobalState {
   readonly error?: DetailedError;
-  readonly keyShortcutModalVisible?: boolean;
 }
 
 const initialState: IGlobalState = {
   error: undefined,
-  keyShortcutModalVisible: false,
 };
 
 enum GlobalActions {
   SET_ERROR = "GlobalActions.SET_ERROR",
-  SET_KEY_SHORTCUT_MODAL_VISIBLE = "GlobalActions.SET_KEY_SHORTCUT_MODAL_VISIBLE",
 }
 
 function setError(error: DetailedError): PayloadAction {
   return {
     type: GlobalActions.SET_ERROR,
     payload: { error },
-  };
-}
-
-function setKeyShortcutModalVisible(keyShortcutModalVisible: boolean): PayloadAction {
-  return {
-    type: GlobalActions.SET_KEY_SHORTCUT_MODAL_VISIBLE,
-    payload: { keyShortcutModalVisible },
   };
 }
 
@@ -38,15 +28,9 @@ function globalReducer(state = initialState, action: PayloadAction): IGlobalStat
         error: action.payload.error,
       };
 
-    case GlobalActions.SET_KEY_SHORTCUT_MODAL_VISIBLE:
-      return {
-        ...state,
-        keyShortcutModalVisible: action.payload.keyShortcutModalVisible,
-      };
-
     default:
       return state;
   }
 }
 
-export { IGlobalState, GlobalActions, globalReducer, setError, setKeyShortcutModalVisible };
+export { IGlobalState, GlobalActions, globalReducer, setError };
