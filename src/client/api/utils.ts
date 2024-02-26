7fb626b286fac1886a50f781febbb74b1b9422b9
Mark Ormesher <me@markormesher.co.uk>
2024-02-26 08:12:28 +0000

    add comment

--- src/client/api/utils.ts
+++ src/client/api/utils.ts
@@ -3,14 +3,16 @@ import { globalErrorManager } from "../helpers/errors/error-manager";
 
 const cache: Map<string, unknown> = new Map<string, unknown>();
 
-function cacheWrap<T>(key: string, f: () => Promise<T>): () => [T | undefined, () => void] {
+// provides a means to access the cached return value of an expensive function, and update that value if required
+// useful on the front-end for API calls to show a value immediately, then update it shortly after
+function cacheWrap<T>(key: string, fetchData: () => Promise<T>): () => [T | undefined, () => void] {
   return () => {
     const cachedValue: T | undefined = cache.has(key) ? (cache.get(key) as T) : undefined;
 
     const [state, setState] = useState<T | undefined>(cachedValue);
 
     const refresh = () => {
-      f()
+      fetchData()
         .then((v) => {
           cache.set(key, v);
           setState(v);
