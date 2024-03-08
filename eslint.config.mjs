33586f53b002e914bd482ccee67461a279011b1d
Mark Ormesher <me@markormesher.co.uk>
2024-03-08 18:31:33 +0000

    fix lint execution

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,9 @@
 import config from "@markormesher/eslint-config";
 
+for (let i = 0; i < config.length; ++i) {
+  if (config[i].languageOptions?.parserOptions?.project) {
+    config[i].languageOptions.parserOptions.project = "./tsconfig.all.json";
+  }
+}
+
 export default config;
