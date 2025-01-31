207d10d9eda38500ce2035602e535140a1f9eb07
Mark Ormesher <me@markormesher.co.uk>
2025-01-31 20:47:32 +0000

    better profile logic: remove if no role, auto create, auto select

--- backend/internal/core/core.go
+++ backend/internal/core/core.go
@@ -3,7 +3,10 @@ package core
 import (
 	"github.com/markormesher/money-dashboard/internal/config"
 	"github.com/markormesher/money-dashboard/internal/database"
+	"github.com/markormesher/money-dashboard/internal/logging"
 )
+
+var l = logging.Logger
 
 type Core struct {
 	DB     *database.DB
--- backend/internal/core/users.go
+++ backend/internal/core/users.go
@@ -16,11 +16,13 @@ func (c *Core) GetOrCreateUser(ctx context.Context, externalUsername string, dis
 
 	// create a new user if one did not exist
 	if !ok {
-		return c.createNewUserAndDefaultProfile(ctx, externalUsername, displayName)
+		l.Info("creating a new user", "externalUsername", externalUsername)
+		return c.createNewUser(ctx, externalUsername, displayName)
 	}
 
 	// update the existing user if necessary
 	if user.DisplayName != displayName {
+		l.Debug("updating user display name", "userId", user.ID)
 		user.DisplayName = displayName
 		err := c.DB.UpsertUser(ctx, user)
 		if err != nil {
@@ -28,10 +30,52 @@ func (c *Core) GetOrCreateUser(ctx context.Context, externalUsername string, dis
 		}
 	}
 
+	// load profiles for this user and create a default one if they don't have any
+	profiles, err := c.DB.GetUserProfiles(ctx, user.ID)
+	if err != nil {
+		return schema.User{}, err
+	}
+
+	if len(profiles) == 0 {
+		l.Info("user has no profiles - creating a default one", "userId", user.ID)
+		defaultProfile, err := c.createDefaultProfile(ctx, user)
+		if err != nil {
+			return schema.User{}, err
+		}
+
+		profiles = append(profiles, defaultProfile)
+	}
+
+	// unset the active profile if the user no longer has a role on it
+	if user.ActiveProfile != nil {
+		hasAccess := false
+		for _, p := range profiles {
+			if p.ID == user.ActiveProfile.ID {
+				hasAccess = true
+				break
+			}
+		}
+
+		if !hasAccess {
+			l.Info("user no longer has access to their active profiles - removing it", "userId", user.ID)
+			user.ActiveProfile = nil
+		}
+	}
+
+	// make sure a profile is set (it might have been removed earlier)
+	if user.ActiveProfile == nil {
+		l.Info("user has no active profile - picking one for them", "userId", user.ID)
+		err := c.SetActiveProfile(ctx, user, profiles[0].ID)
+		if err != nil {
+			return schema.User{}, err
+		}
+		user.ActiveProfile = &profiles[0]
+	}
+
 	return user, nil
 }
 
-func (c *Core) createNewUserAndDefaultProfile(ctx context.Context, externalUsername string, displayName string) (schema.User, error) {
+func (c *Core) createNewUser(ctx context.Context, externalUsername string, displayName string) (schema.User, error) {
 	// create the user
 	user := schema.User{
 		ID:               uuid.New(),
@@ -44,22 +88,7 @@ func (c *Core) createNewUserAndDefaultProfile(ctx context.Context, externalUsern
 	}
 
 	// create the profile
-	profile := schema.Profile{
-		ID:   uuid.New(),
-		Name: "Default",
-	}
-	err = c.DB.UpsertProfile(ctx, profile)
-	if err != nil {
-		return schema.User{}, err
-	}
-
-	// make the user the owner of the profile
-	role := schema.UserProfileRole{
-		User:    &user,
-		Profile: &profile,
-		Role:    "owner",
-	}
-	err = c.DB.UpsertUserProfileRole(ctx, role)
+	profile, err := c.createDefaultProfile(ctx, user)
 	if err != nil {
 		return schema.User{}, err
 	}
@@ -72,6 +101,31 @@ func (c *Core) createNewUserAndDefaultProfile(ctx context.Context, externalUsern
 	user.ActiveProfile = &profile
 
 	return user, nil
+}
+
+func (c *Core) createDefaultProfile(ctx context.Context, user schema.User) (schema.Profile, error) {
+	// create the profile
+	profile := schema.Profile{
+		ID:   uuid.New(),
+		Name: "Default",
+	}
+	err := c.DB.UpsertProfile(ctx, profile)
+	if err != nil {
+		return schema.Profile{}, err
+	}
+
+	// make the user the owner of the profile
+	role := schema.UserProfileRole{
+		User:    &user,
+		Profile: &profile,
+		Role:    "owner",
+	}
+	err = c.DB.UpsertUserProfileRole(ctx, role)
+	if err != nil {
+		return schema.Profile{}, err
+	}
+
+	return profile, nil
 }
 
 func (c *Core) GetProfiles(ctx context.Context, user schema.User) ([]schema.Profile, error) {
