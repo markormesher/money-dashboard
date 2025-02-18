4b9903cbff05aef7bac0ed1e6b7e53d099b81f86
Mark Ormesher <me@markormesher.co.uk>
2025-02-18 20:16:58 +0000

    remove duplicated annotations

--- backend/internal/database/conversion/src.go
+++ backend/internal/database/conversion/src.go
@@ -11,45 +11,38 @@ import (
 // goverter:output:file ./generated.go
 // goverter:output:package github.com/markormesher/money-dashboard/internal/database/conversion
 // goverter:matchIgnoreCase yes
+// goverter:ignoreUnexported yes
+// goverter:ignoreMissing yes
 // goverter:skipCopySameType yes
 // goverter:useZeroValueOnPointerInconsistency yes
 // goverter:extend ConvertPgTextToPrimitive
 // goverter:extend ConvertPgIntToPrimitive
 // goverter:extend ConvertPgBoolToPrimitive
 type converterSpec interface {
-
-	// goverter:ignore Currency
 	AssetToCore(source database_gen.Asset) schema.Asset
 
-	// goverter:ignore Profile
 	AccountToCore(source database_gen.Account) schema.Account
 
 	AssetPriceToCore(source database_gen.AssetPrice) schema.AssetPrice
 
-	// goverter:ignore Profile
 	CategoryToCore(source database_gen.Category) schema.Category
 
 	CurrencyToCore(source database_gen.Currency) schema.Currency
 
 	CurrencyRateToCore(source database_gen.CurrencyRate) schema.CurrencyRate
 
-	// goverter:ignore Currency Asset Account Profile
 	HoldingToCore(source database_gen.Holding) schema.Holding
 
 	NullableHoldingCurrencyToCore(source database_gen.NullableHoldingCurrency) schema.Currency
 
-	// goverter:ignore Currency
 	NullableHoldingAssetToCore(source database_gen.NullableHoldingAsset) schema.Asset
 
 	ProfileToCore(source database_gen.Profile) schema.Profile
 
-	// goverter:ignore Holding Category Profile
 	TransactionToCore(source database_gen.Transaction) schema.Transaction
 
-	// goverter:ignore ActiveProfile
 	UserToCore(source database_gen.Usr) schema.User
 
-	// goverter:ignore User Profile
 	UserProfileRoleToCore(source database_gen.UserProfileRole) schema.UserProfileRole
 }
 
