bac44f33017692419ac01bdce3f2257a8b346fbf
Mark Ormesher <me@markormesher.co.uk>
2025-03-10 21:37:37 +0000

    build out balances view on dashboard

--- frontend/src/app/components/dashboard/holding-balances-tile.tsx
+++ frontend/src/app/components/dashboard/holding-balances-tile.tsx
@@ -5,11 +5,26 @@ import { reportingServiceClient } from "../../../api/api.js";
 import { toastBus } from "../toaster/toaster.js";
 import { ErrorPanel } from "../common/error/error.js";
 import { LoadingPanel } from "../common/loading/loading.js";
+import { concatClasses } from "../../utils/style.js";
+import { Icon, IconGroup } from "../common/icon/icon.js";
+import { Account, AccountGroup } from "../../../api_gen/moneydashboard/v4/accounts_pb.js";
+import { formatCurrency } from "../../utils/currency.js";
+import "./holding-balances-tile.css";
+
+type Group = {
+  accountGroup: AccountGroup;
+  rows: Record<string, GroupRow>;
+};
+
+type GroupRow = {
+  account: Account;
+  balances: HoldingBalance[];
+  balanceSum: number;
+};
 
 function HoldingBalancesTile(): ReactElement {
   const [error, setError] = React.useState<unknown>();
   const [holdingBalances, setHoldingBalances] = React.useState<HoldingBalance[]>();
-
   useAsyncEffect(async () => {
     try {
       const res = await reportingServiceClient.getHoldingBalances({});
@@ -21,24 +36,104 @@ function HoldingBalancesTile(): ReactElement {
     }
   }, []);
 
+  const [showHoldings, setShowHoldings] = React.useState(false);
+
   if (error) {
     return <ErrorPanel error={error} />;
   } else if (!holdingBalances) {
     return <LoadingPanel />;
   }
 
+  const groups: Record<string, Group> = {};
+
+  for (const hb of holdingBalances) {
+    const account = hb.holding?.account;
+    const accountGroup = account?.accountGroup;
+    if (!account || !accountGroup) {
+      // we don't expect this to actually happen, it just makes the typing cleaner below
+      continue;
+    }
+
+    let group = groups[accountGroup.id];
+    if (!group) {
+      group = { accountGroup, rows: {} };
+      groups[accountGroup.id] = group;
+    }
+
+    let row = group.rows[account.id];
+    if (!row) {
+      row = { account, balances: [], balanceSum: 0 };
+      group.rows[account.id] = row;
+    }
+
+    row.balanceSum += hb.gbpBalance;
+    row.balances.push(hb);
+  }
+
   return (
     <article>
-      <ul>
-        {holdingBalances.map((b) => {
+      <header>
+        <h4 className={"mb0"}>
+          <IconGroup>
+            <Icon name={"account_balance"} />
+            <span>Account Balances</span>
+          </IconGroup>
+        </h4>
+      </header>
+
+      {Object.values(groups)
+        .sort((a, b) => a.accountGroup.displayOrder - b.accountGroup.displayOrder)
+        .map((g) => {
           return (
-            <li>
-              {b.holding?.account?.name} / {b.holding?.name} / {b.rawBalance} ( = {b.gbpBalance})
-            </li>
+            <>
+              <h6>{g.accountGroup.name}</h6>
+              {Object.values(g.rows)
+                .sort((a, b) => Math.abs(b.balanceSum) - Math.abs(a.balanceSum))
+                .map((r) => {
+                  if (!showHoldings || r.balances.length == 1) {
+                    return (
+                      <div className={"balance-row"}>
+                        <div className={"row-label"}>{r.account.name}</div>
+                        <div className={"row-value"}>{formatCurrency(r.balanceSum, null)}</div>
+                      </div>
+                    );
+                  }
+
+                  return (
+                    <div className={concatClasses("balance-row", "with-children")}>
+                      <div className={"row-label"}>{r.account.name}</div>
+                      <div className={"row-children"}>
+                        {r.balances
+                          .sort((a, b) => b.gbpBalance - a.gbpBalance)
+                          .map((b) => {
+                            return (
+                              <div className={"balance-row"}>
+                                <div className={"row-label"}>{b.holding?.name}</div>
+                                <div className={"row-value"}>{formatCurrency(b.gbpBalance, null)}</div>
+                              </div>
+                            );
+                          })}
+                      </div>
+                    </div>
+                  );
+                })}
+            </>
           );
         })}
-        <li>Total = {holdingBalances.map((b) => b.gbpBalance).reduce((a, b) => a + b)}</li>
-      </ul>
+
+      <footer>
+        <fieldset className={concatClasses("mb0", "muted")}>
+          <label>
+            <input
+              type={"checkbox"}
+              role={"switch"}
+              checked={showHoldings}
+              onChange={(evt) => setShowHoldings(evt.target.checked)}
+            />
+            Individual holdings
+          </label>
+        </fieldset>
+      </footer>
     </article>
   );
 }
--- frontend/src/config/consts.ts
+++ frontend/src/config/consts.ts
@@ -1,7 +1,19 @@
+import { Currency } from "../api_gen/moneydashboard/v4/currencies_pb.js";
+
 const GBP_CURRENCY_ID = "b3092a40-1802-46fd-9967-11c7ac3522c5";
+
+const GBP_CURRENCY: Currency = {
+  $typeName: "moneydashboard.v4.Currency",
+  id: GBP_CURRENCY_ID,
+  code: "GBP",
+  symbol: "£",
+  displayPrecision: 2,
+  calculationPrecision: 4,
+  active: true,
+};
 
 const NULL_UUID = "00000000-0000-0000-0000-000000000000";
 
 const PLATFORM_MINIMUM_DATE = new Date("2015-04-06");
 
-export { GBP_CURRENCY_ID, NULL_UUID, PLATFORM_MINIMUM_DATE };
+export { GBP_CURRENCY_ID, GBP_CURRENCY, NULL_UUID, PLATFORM_MINIMUM_DATE };
