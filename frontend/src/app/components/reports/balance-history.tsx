2df6f32990790822b4103850ba3e2ba853b2d56b
Mark Ormesher <me@markormesher.co.uk>
2025-04-20 17:18:37 +0100

    balance history chart

--- frontend/src/app/components/reports/balance-history.tsx
+++ frontend/src/app/components/reports/balance-history.tsx
@@ -1,5 +1,17 @@
 import React, { ReactElement } from "react";
 import { subMonths } from "date-fns";
+import {
+  ChartData,
+  Chart as ChartJS,
+  ChartOptions,
+  Filler,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Tooltip,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { isNumber } from "chart.js/helpers";
 import { useRouter } from "../app/router.js";
 import { PageHeader } from "../page-header/page-header.js";
 import { Icon, IconGroup } from "../common/icon/icon.js";
@@ -8,10 +20,15 @@ import { DateRangePicker } from "../common/date-range/date-range-picker.js";
 import { BalanceHistoryEntry } from "../../../api_gen/moneydashboard/v4/reporting_pb.js";
 import { useAsyncEffect, useWaitGroup } from "../../utils/hooks.js";
 import { reportingServiceClient } from "../../../api/api.js";
-import { convertDateToProto, parseDateFromProto } from "../../utils/dates.js";
+import { convertDateToProto, formatDateFromProto } from "../../utils/dates.js";
 import { toastBus } from "../toaster/toaster.js";
 import { ErrorPanel } from "../common/error/error.js";
 import { LoadingPanel } from "../common/loading/loading.js";
+import { formatCurrencyValue, formatCurrencyValueAsMagnitude } from "../../utils/currency.js";
+import "./reports.css";
+import { concatClasses } from "../../utils/style.js";
+
+ChartJS.register(Tooltip, LinearScale, PointElement, LineElement, Filler);
 
 function BalanceHistoryPage(): ReactElement {
   const { setMeta } = useRouter();
@@ -60,18 +77,75 @@ function BalanceHistoryPage(): ReactElement {
   } else if (!data) {
     body = <LoadingPanel />;
   } else {
-    body = (
-      <pre>
-        {JSON.stringify(
-          data.map((b) => ({
-            date: parseDateFromProto(b.date),
-            balance: b.gbpBalance,
-          })),
-          null,
-          2,
-        )}
-      </pre>
-    );
+    const chartOptions: ChartOptions<"line"> = {
+      responsive: true,
+      animation: false,
+      maintainAspectRatio: false,
+      interaction: {
+        intersect: false,
+        mode: "nearest",
+      },
+      plugins: {
+        tooltip: {
+          callbacks: {
+            title: (items) => {
+              const idx = items[0]?.dataIndex;
+              if (idx !== undefined && idx >= 0 && idx < data.length) {
+                return formatDateFromProto(data[idx]?.date);
+              } else {
+                return "???";
+              }
+            },
+            label: (ctx) => {
+              return formatCurrencyValue(ctx.parsed.y, null);
+            },
+          },
+        },
+      },
+      scales: {
+        x: {
+          type: "linear",
+          bounds: "data",
+          ticks: {
+            callback: (val) => {
+              if (isNumber(val)) {
+                return formatDateFromProto(BigInt(val));
+              } else {
+                return "???";
+              }
+            },
+          },
+        },
+        y: {
+          type: "linear",
+          beginAtZero: true,
+          ticks: {
+            callback: (val) => {
+              if (isNumber(val)) {
+                return formatCurrencyValueAsMagnitude(val);
+              } else {
+                return "???";
+              }
+            },
+          },
+        },
+      },
+    };
+
+    const chartData: ChartData<"line"> = {
+      labels: data.map((e) => Number(e.date)),
+      datasets: [
+        {
+          data: data.map((e) => e.gbpBalance),
+          fill: true,
+          borderColor: "#018CD4", // pico azuer 400
+          backgroundColor: "#9BCCFD", // pico azure 200
+          pointStyle: false,
+        },
+      ],
+    };
+
+    body = <Line options={chartOptions} data={chartData} className={concatClasses(wg.count > 0 && "loading")} />;
   }
 
   return (
--- frontend/src/app/utils/currency.ts
+++ frontend/src/app/utils/currency.ts
@@ -12,4 +12,18 @@ function formatCurrencyValue(amount: number, currency: Currency | null): string 
   });
 }
 
-export { formatCurrencyValue };
+function formatCurrencyValueAsMagnitude(amount: number): string {
+  if (amount < 1_000) {
+    return "£" + amount.toFixed(0);
+  } else if (amount < 1_000_000) {
+    return "£" + (amount / 1_000).toFixed(1) + "K";
+  } else if (amount < 1_000_000_000) {
+    // we can dream
+    return "£" + (amount / 1_000_000).toFixed(1) + "M";
+  } else {
+    // probably a dream too far
+    return "£" + (amount / 1_000_000_000).toFixed(1) + "B";
+  }
+}
+
+export { formatCurrencyValue, formatCurrencyValueAsMagnitude };
